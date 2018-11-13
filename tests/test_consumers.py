8f9be667b01025377cec191aab9aa9d049731e3e
Jairo Vadillo <jairo.vadillo@gmail.com>
2018-11-13 16:58:29 +0100

    Add message mapper (#7)

--- tests/test_consumers.py
+++ tests/test_consumers.py
@@ -7,20 +7,67 @@ from molange.event_bus import event_bus
 
 
 class TestConsumer:
-    def test_message_processed(self):
-        event_mock = self._get_event_mock('test_event')
-        subscriber_mock = self._get_subscriber_mock(event_mock)
-        event_factory_mock = self._generate_event_factory(event_mock)
-        queue_driver_mock = self._get_queue_driver_mock(event_mock.name)
+    def test_event_triggered(self):
+        message_name = 'test_message'
+        message = Message(event_type_name=message_name, body={'foo': 21})
+        queue_driver_mock = self._get_queue_driver_mock(return_value=message)
 
-        event_bus.subscribe(subscriber_mock)
+        event_name = 'test_event'
+        event = self._get_event_mock(event_name)
+        mapper_factory = {
+            message_name: lambda x: event
+        }
 
-        consumer = MessageConsumer(event_factory=event_factory_mock,
+        event_bus_mock = MagicMock()
+
+        consumer = MessageConsumer(mapper_factory=mapper_factory,
+                                   event_bus=event_bus_mock,
+                                   queue_driver=queue_driver_mock)
+        consumer.process_message()
+
+        event_bus_mock.trigger.assert_called_once_with(event)
+
+    def test_mapper_called(self):
+        message_name = 'test_message'
+        message = Message(event_type_name=message_name, body={'foo': 21})
+        queue_driver_mock = self._get_queue_driver_mock(return_value=message)
+
+        mapper = MagicMock()
+        mapper_factory = {
+            message_name: mapper
+        }
+
+        consumer = MessageConsumer(mapper_factory=mapper_factory,
                                    event_bus=event_bus,
                                    queue_driver=queue_driver_mock)
         consumer.process_message()
 
-        subscriber_mock.run.assert_called_once()
+        mapper.assert_called_once_with(message)
+
+    def test_message_not_received(self):
+        mapper_factory = {}
+        queue_driver_mock = self._get_queue_driver_mock()
+
+        consumer = MessageConsumer(mapper_factory=mapper_factory,
+                                   event_bus=event_bus,
+                                   queue_driver=queue_driver_mock)
+        consumer.process_message()
+
+        queue_driver_mock.move_message_to_dead_letter_queue.assert_not_called()
+        queue_driver_mock.delete_message.assert_not_called()
+
+    def test_dead_letter_queue_when_missing_mapping(self):
+        message = Message(event_type_name='test_message', body={'foo': 21})
+        queue_driver_mock = self._get_queue_driver_mock(return_value=message)
+
+        mapper_factory = {}
+
+        consumer = MessageConsumer(mapper_factory=mapper_factory,
+                                   event_bus=event_bus,
+                                   queue_driver=queue_driver_mock)
+        consumer.process_message()
+
+        queue_driver_mock.move_message_to_dead_letter_queue.assert_called_once_with(message)
 
     def _get_event_mock(self, event_name):
         event_mock = MagicMock()
@@ -34,13 +81,8 @@ class TestConsumer:
 
         return subscriber_mock
 
-    def _generate_event_factory(self, event):
-        return {
-            event.name: event
-        }
-
-    def _get_queue_driver_mock(self, event_name) -> GenericDriver:
+    def _get_queue_driver_mock(self, return_value=None):
         queue_driver = MagicMock(spec=GenericDriver)
-        queue_driver.receive_message.return_value = Message(event_type_name=event_name, body={'foo': 21})
+        queue_driver.receive_message.return_value = return_value
 
         return queue_driver
