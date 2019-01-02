4744c449bed1fa92426c54b5b08ecd6072abbddf
Jairo Vadillo <jairo.vadillo@gmail.com>
2019-01-02 15:56:27 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+
+import io
+import os
+
+from setuptools import setup
+
+NAME = 'molange'
+DESCRIPTION = 'SNS + SQS Communications made easy'
+URL = f'https://github.com/21buttons/{NAME}'
+EMAIL = 'jairo@21buttons.com'
+AUTHOR = '21 Buttons APP S.L.'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = [
+    'boto3>=1.9.40,<1.10'
+]
+
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=[NAME],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="Apache License 2.0",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
